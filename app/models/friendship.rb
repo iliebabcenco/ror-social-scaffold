bf81265157ea0185686ad8ef9354521d0d4e4319
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-04-30 11:25:54 +0300

    Add friendship migration, associations, methods for friends logic in user model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,8 @@
 class Friendship < ApplicationRecord
-  belongs_to :user
-  belongs_to :friend, class_name: "User"
+  belongs_to :initiator, class_name: "User"
+  belongs_to :invitee, class_name: "User"
+
+  scope :pending, -> { where(confirmed: false) }
+  scope :confirmed, -> { where(confirmed: true) }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,41 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  scope :all_friends, 
+  # has_many :friendships
+  # has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
 
-  has_many :friendships
-  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :initiated_friendships, foreign_key: :initiator_id, class_name: 'Friendship'
+  has_many :friends_as_initiator, through: :initiated_friendships, source: :invitee
+
+  has_many :invited_friendships, foreign_key: :invitee_id, class_name: 'Friendship'
+  has_many :friends_as_invitee, through: :invited_friendships, source: :initiator
+
+  def friends
+    friends_as_initiator.merge(Friendship.confirmed) + friends_as_invitee.merge(Friendship.confirmed)
+  end
+
+  def pending_friends
+    friends_as_initiator.merge(Friendship.pending) + friends_as_invitee.merge(Friendship.pending)
+  end
+
+  def sent_requests
+    friends_as_initiator.merge(Friendship.pending)
+  end
+
+  def received_requests
+    friends_as_invitee.merge(Friendship.pending)
+  end
+
+  def confirm_friend(id)
+    friendship = invited_friendships.find_by_initiator_id(id)
+    unless friendship.nil?
+      friendship.confirmed = true
+      friendship.save
+    end
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
+
 end
